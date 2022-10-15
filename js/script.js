5e652f36e007e98e10b743f3c0ea4a457d5dc68f
Maksym Solomko <max.exc7@gmail.com>
2022-10-16 01:45:41 +0300

    feature detection / crossbrowser

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-const lazyImages = document.querySelectorAll("img"); // img[loading="lazy"]
-
-console.log(lazyImages);
-
-lazyImages.forEach((image) => {
-  image.addEventListener("load", onImageLoaded, { once: true });
-});
-
-function onImageLoaded(evt) {
-  console.log("Picture loaded");
-  //   console.log(evt);
-  console.log(evt.target);
-  evt.target.classList.add("appear");
-}
-
-// https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js
-
 if ("loading" in HTMLImageElement.prototype) {
   console.log("Browser support LazyLoad))");
   // supported in browser
@@ -39,3 +22,20 @@ if ("loading" in HTMLImageElement.prototype) {
 
   document.body.appendChild(script);
 }
+
+const lazyImages = document.querySelectorAll("img"); // img[loading="lazy"]
+
+console.log(lazyImages);
+
+lazyImages.forEach((image) => {
+  image.addEventListener("load", onImageLoaded, { once: true });
+});
+
+function onImageLoaded(evt) {
+  console.log("Picture loaded");
+  //   console.log(evt);
+  console.log(evt.target);
+  evt.target.classList.add("appear");
+}
+
+// https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js
